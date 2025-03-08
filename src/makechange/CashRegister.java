eb30444279d502ea8996772ebc177294aa55a03d
Jinyoung Yoon <jyoo7375@gmail.com>
2025-03-07 20:31:11 -0500

    modified code

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -29,22 +29,32 @@ public class CashRegister {
 			changeDue = amountPaid - amountDue;
 			
 		int twentyOwed = (int)(changeDue / 20);
-		changeDue = (int) changeDue - (twentyOwed * 20);
+		changeDue -= twentyOwed * 20;
 		
 		int tensOwed = (int)(changeDue / 10);
-		changeDue = (int) changeDue - (tensOwed * 10);
+		changeDue -= tensOwed * 10;
 		
 		int fivesOwed = (int)(changeDue / 5);
-		changeDue = (int) changeDue - (fivesOwed * 5);
+		changeDue -= fivesOwed * 5;
 		
 		int onesOwed = (int)(changeDue / 1);
-		changeDue = (int)(changeDue - onesOwed);
+		changeDue -= onesOwed;
 		
 		changeDue = changeDue * 100.0;
 		
 		int quarterOwed = (int)(changeDue / 25);
-		changeDue = (int) changeDue - (quarterOwed * 25);
-			System.out.println(twentyOwed + " and " + fivesOwed + " and " + onesOwed);
+		changeDue -= quarterOwed * 25;
+		
+		int dimeOwed = (int)(changeDue / 10);
+		changeDue -= dimeOwed * 10;
+		
+		int nickelOwed = (int)(changeDue / 5);
+		changeDue -= nickelOwed * 5;
+		
+		int pennyOwed = (int)(changeDue / 1);
+		changeDue -= pennyOwed * 1;
+			System.out.println("Your change is " + twentyOwed + " twenty dollar(s) " + tensOwed + " ten dollar(s) " + fivesOwed + " five dollar(s) " + onesOwed + " one dollar(s).");
+			System.out.println(quarterOwed + " quarters " + dimeOwed + " dimes " + nickelOwed + " nickels " + pennyOwed + " pennies.");
 		} else if(amountPaid == amountDue) {
 			System.out.println("Thank you for purchasing there will be no change.");
 		}
